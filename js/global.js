708c56a86389998bb13f63e4a467c6c74b805875
Korgan00 <korgan00@gmail.com>
2016-04-08 01:57:19 +0200

    initial page push

---
+++ js/global.js
@@ -0,0 +1,37 @@
+/*
+(function () {
+  "use strict";
+  var toggles = document.querySelectorAll(".menuButton");
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active"): this.classList.add("is-active");
+    });
+  }
+})();
+*/
+
+(function() {
+  function loadJSON(file, callback) {
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+      }
+    };
+    xobj.send(null);  
+  }
+  function init() {
+    loadJSON("/data/news.json", function(response) {
+      var actual_JSON = JSON.parse(response);
+    });
+  }
+})
